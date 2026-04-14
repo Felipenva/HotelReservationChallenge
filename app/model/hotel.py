d86bc27bd544f7326a301052b7321b9b87f3dec9
Maria Vargas <majovargas0710@gmail.com>
2026-04-13 19:04:35 -0500

    feat: implement Guest class

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -6,14 +6,25 @@ from app.services.util import (generate_unique_id, date_lower_than_today_error,
     reservation_not_found_error, guest_not_found_error, room_not_available_error,
     room_not_found_error, room_already_exists_error)
 
-
 # TODO: Implement Guest class here
+@dataclass
+class Guest:
+    REGULAR = "regular"
+    VIP = "vip"
+
+    name: str
+    email: str
+    type_: str = REGULAR
+
+    def __str__(self):
+        return f"Guest {self.name} ({self.email}) of type {self.type_}"
 
 
 # TODO: Implement Reservation class here
 
 
 # TODO: Implement Room class here
+
 
 
 # TODO: Implement Hotel class here
